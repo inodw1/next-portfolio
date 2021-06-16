070159d50984ab93610700053e27c9c876addea7
inod-noetic <inod.wagachchi@wearenoetic.com>
2021-06-16 23:11:03 +0530

    Add comment after replace the url by router :bulb:

--- pages/post.js
+++ pages/post.js
@@ -1,6 +1,13 @@
 import Layout from "../components/Layout"
 import { withRouter } from "next/router"
 
+/**
+ * url is deprecated
+ * Since Next 5  they provide a way to explicitly inject the Next.js router object into pages and all their components
+ * The router property that is injected will hold the same value as url, like pathname, asPath, and query
+ * @param {*} param0 
+ * @returns 
+ */
 const Post = ({ router }) => (
     <Layout title={router.query.title}>
         <p style={{ width: "80vw" }}>
